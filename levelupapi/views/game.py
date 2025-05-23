c4be0de70276954dd3be12601815f6e966c89848
Tyler Davenport <tydavenport0415@gmail.com>
2025-05-22 20:50:16 -0500

    added create to event and game

--- levelupapi/views/game.py
+++ levelupapi/views/game.py
@@ -2,7 +2,7 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from levelupapi.models import Game
+from levelupapi.models import Game, GameType, Gamer
 
 
 class GameView(ViewSet):
@@ -26,6 +26,22 @@ class GameView(ViewSet):
 
         serializer = GameSerializer(games, many=True)
         return Response(serializer.data)
+
+    def create(self, request):
+        gamer = Gamer.objects.get(uid=request.data["userId"])
+        game_type = GameType.objects.get(pk=request.data["gameType"])
+
+        game = Game.objects.create(
+            title=request.data["title"],
+            maker=request.data["maker"],
+            number_of_players=request.data["numberOfPlayers"],
+            skill_level=request.data["skillLevel"],
+            game_type=game_type,
+            gamer=gamer,
+        )
+
+        serializer = GameSerializer(game)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class GameSerializer(serializers.ModelSerializer):
