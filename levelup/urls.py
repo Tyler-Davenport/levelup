0ac7562e8ec4b4658def2c0af6c89da6a0dae351
Tyler Davenport <tydavenport0415@gmail.com>
2025-05-24 18:48:25 -0500

    added view for register and check_user

--- levelup/urls.py
+++ levelup/urls.py
@@ -19,6 +19,8 @@ from rest_framework import routers
 from levelupapi.views import GameTypeView, EventView, GameView
 from django.contrib import admin
 from django.urls import path
+from django.urls import path
+from levelupapi.views import register_user, check_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r"gametypes", GameTypeView, "gametype")
@@ -27,4 +29,6 @@ router.register(r"games", GameView, "game")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
+    path("register", register_user),
+    path("checkuser", check_user),
 ]
